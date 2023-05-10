f4fbfc85b21f2ec7c2c0a2d173e775d019343911
Braunerr <mathn519@gmail.com>
2023-05-10 09:58:09 +0200

    added logging

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 using ServiceWorker;
+using NLog;
+using NLog.Web;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
     })
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+    })
+    .UseNLog()
     .Build();
 
-host.Run();
+await host.RunAsync();
